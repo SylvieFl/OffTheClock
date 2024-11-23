e0b80a4ef0285a454b473ddf726c5dc6daa33dd5
SylvieFl <160358011+SylvieFl@users.noreply.github.com>
2024-11-23 15:41:29 -0800

    Jump and Dash animations
    I have jump and dash animations sorta working.

--- OffTheClock/Assets/Scripts/PlayerMovement.cs
+++ OffTheClock/Assets/Scripts/PlayerMovement.cs
@@ -64,10 +64,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Dash(float input)
     {
+        StopAllCoroutines();
         xInput = 0;
         yInput = 0;
         body.gravityScale = 0;
         gravityDecay = 0;
+        animator.SetBool("isDashing", true);
         body.AddForce(new Vector2(input, 0) * dashForce, ForceMode2D.Impulse);
         inControl = false;
         canDash = false;
@@ -92,13 +94,22 @@ public class PlayerMovement : MonoBehaviour
             if (grounded)
             {
                 body.velocity = new Vector2(body.velocity.x, yInput * jumpSpeed);
+                animator.SetBool("isJumping", true);
+                StartCoroutine(JumpEnd());
             }
             else if (canDoubleJump)
             {
+                StopCoroutine(JumpEnd());
+                
+                //StopAllCoroutines();
+                animator.SetBool("isJumping", false);
+                StartCoroutine(DoubleJump());
+                
                 body.velocity = new Vector2(body.velocity.x, yInput * jumpSpeed);
                 canDoubleJump = false;
+                
             }
-
+            
         }
     }
 
@@ -129,6 +140,28 @@ public class PlayerMovement : MonoBehaviour
         gravityDecay = 1;
         inControl = true;
         trailRenderer.emitting = false;
+    }
+
+    IEnumerator JumpEnd()
+    {
+
+        yield return new WaitForSeconds(1f);
+
+        animator.SetBool("isJumping", false);
+
+    }
+
+    IEnumerator DoubleJump()
+    {
+
+        yield return new WaitForSeconds(0.01f);
+
+        animator.SetBool("isJumping", true);
+
+        yield return new WaitForSeconds(1f);
+
+        animator.SetBool("isJumping", false);
+
     }
 
     private void FlipCharacter()
