49bcf33b6c1cd2f7997c966965ebcfe679875dca
iroot07 <165845687+iroot07@users.noreply.github.com>
2024-11-23 08:58:40 -0800

    Fixed bug
    Fixed bug

--- OffTheClock/Assets/Bullet.cs
+++ OffTheClock/Assets/Bullet.cs
@@ -8,6 +8,7 @@ public class Bullet : MonoBehaviour
     private Camera mainCam;
     private Rigidbody2D rb;
     public float force;
+    //public Transform target;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,7 @@ public class Bullet : MonoBehaviour
         //mainCam = GameObject.
         rb = GetComponent<Rigidbody2D>();
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - transform.position;
+        Vector3 direction = mousePos - GameObject.Find("Player").transform.position;
         Vector3 rotation = transform.position - mousePos;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
