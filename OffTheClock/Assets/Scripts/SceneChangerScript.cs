90b68914c2a5cd58b8840014851077a75bde551b
SylvieFl <160358011+SylvieFl@users.noreply.github.com>
2024-11-22 22:06:01 -0800

    Start Screen with Button
    I created a start screen with a clickable start game button that switches the scene to the level 1 scene.

---
+++ OffTheClock/Assets/Scripts/SceneChangerScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerScript : MonoBehaviour
+{
+  
+    public void ChangeScene(int buildIndex)
+    {
+
+        SceneManager.LoadScene(buildIndex);
+
+    }
+
+}
