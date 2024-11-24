55a550da14392e5848a9f3cce4ee00d55ec0c771
iroot07 <165845687+iroot07@users.noreply.github.com>
2024-11-24 15:14:51 -0800

    Fixed yellow level

--- OffTheClock/Assets/Scripts/DoubleJump.cs
+++ OffTheClock/Assets/Scripts/DoubleJump.cs
@@ -30,14 +30,16 @@ public class DoubleJump : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Enter");
         if (collision.gameObject.CompareTag("Player"))
         {
             inContact = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D collision)
     {
+        Debug.Log("Exit");
         if (collision.gameObject.CompareTag("Player"))
         {
             inContact = false;
