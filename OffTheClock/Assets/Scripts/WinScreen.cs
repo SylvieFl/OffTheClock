354ab2fd461a8bbedee4ecd3838755cccd847691
SylvieFl <160358011+SylvieFl@users.noreply.github.com>
2024-11-24 12:04:12 -0800

    Win Screen Implemented

---
+++ OffTheClock/Assets/Scripts/WinScreen.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+
+    public RawImage winScreen;
+    public Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CoFlipFrames());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CoFlipFrames()
+    {
+        yield return new WaitForSeconds(3.25f);
+        winScreen.enabled = false;
+
+        yield return new WaitForSeconds(3);
+        quitButton.gameObject.SetActive(true);
+
+    }
+
+    public void QuitButton()
+    {
+
+        SceneManager.LoadScene(0);
+        //Application.Quit();
+
+    }
+}
