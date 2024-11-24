439561db6a4d52b381af7bc66dedda7393153be0
iroot07 <165845687+iroot07@users.noreply.github.com>
2024-11-24 13:58:50 -0800

    CoffeeUI

--- OffTheClock/Assets/Scripts/DoubleJump.cs
+++ OffTheClock/Assets/Scripts/DoubleJump.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoubleJump : MonoBehaviour
 {
@@ -23,6 +24,7 @@ public class DoubleJump : MonoBehaviour
         if (playerMovement != null )
         {
             playerMovement.canDoubleJump = true;
+            GameObject.Find("CoffeeUI").GetComponent<RawImage>().color = Color.white;
             Destroy(gameObject);
         }
     }
